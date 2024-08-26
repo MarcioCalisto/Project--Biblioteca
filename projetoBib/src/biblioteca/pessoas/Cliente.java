69013b019094b0fad5d4a3011e77f99eb2c74dbc
CalixtoTech <103325008+ClxtMf@users.noreply.github.com>
2024-08-26 10:02:10 -0300

    Commit update

--- projetoBib/src/biblioteca/pessoas/Cliente.java
+++ projetoBib/src/biblioteca/pessoas/Cliente.java
@@ -1,5 +1,26 @@
 package biblioteca.pessoas;
 
-public class Cliente {
-    
+/*
+ * A classe Cliente herda de Pessoa e representa um cliente na biblioteca.
+ * Adiciona o atributo para o número de livros emprestados.
+ */
+public class Cliente extends Pessoa {
+    private int numLivroEmprestados; // Número de livros emprestados pelo cliente.
+
+    /*
+     * Construtor para inicializar o nome, CPF e definir o número de livros emprestados como zero.
+     * 
+     * @param nome O nome do cliente.
+     * @param cpf O CPF do cliente.
+     */
+    public Cliente(String nome, String cpf) {
+        super(nome, cpf);       // Chama o construtor da classe base Pessoa.
+        this.numLivroEmprestados = 0; // Inicializa o número de livros emprestados como zero.
+    }
+
+    // Implementação do método abstrato exbirInfo
+    @Override
+    public void exibirInfo() {
+        System.out.println("Cliente: " + getNome() + ", cpf: " + getCpf() + ", Livros emprestados: " + numLivroEmprestados);
+    }
 }
